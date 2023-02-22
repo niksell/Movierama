7de4c6aa7535dcaff5afefcf93751acdef2cc4e5
gsalteris <nikselll@gmail.com>
2023-02-22 08:58:29 -0700

    add db seeder for fake data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Movie;
+use App\Models\User;
+use DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,27 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::factory(100)->create()
+            ->each(function($user) {
+            Movie::create([
+                'user_id' => $user->id,
+                'title' => fake()->text($maxNbChars = 50),
+                'description' => fake()->paragraph()
+            ]);
+
+        });
+        foreach ((range(1, 100)) as $index) {
+            DB::table('likes')->insert(
+                [
+                    'userable_type'=>'App\Models\User',
+                    'userable_id' => rand(1, 100),
+                    'likeable_id' => rand(1, 100),
+                    'likeable_type' => 'App\Models\Movie',
+                    'is_like'=>rand(0, 1),
+                ]
+            );
+    
+        }
     }
 }
